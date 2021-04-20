b43d8da6f2d593abd6ca1c64a9b99eb6ec7a5b9a
9Sometimes <palmwannachat@gmail.com>
2021-04-21 00:35:07 +0700

    update

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"log"
 
 	// "github.com/GoogleCloudPlatform/cloud-builders-community/slackbot/slackbot"
-	"github.com/cnydevelop/slackbot/slackbot/Monitor"
+	"github.com/cnydevelop/slackbot/slackbot/monitor"
 	// "cnydevelop/slackbot/slackbot"
 )
 
